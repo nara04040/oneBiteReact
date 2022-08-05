30e1ea57760a87c86e7dfb6024e8215be75dbea9
Dev.Jin <nara04040@naver.com>
2022-08-05 15:55:14 +0900

    2강 JSX

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import React from 'react';
+// import './App.css';
+
+import MyHeader from "./MyHeader"
 
 function App() {
-  let name = 'nara'
+  const myName = 'nara'
+  const style = {
+    App : {
+      backgroundColor: "black"
+    },
+    h2 : {
+      color: 'red'
+    },
+    bold_text: {
+      color: 'green'
+    }
+  }
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕리액트 {name} </h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+        <h2 style={style.h2}>안녕리액트</h2>
+        <b style={style.bold_text}>
+          {number} : {number % 2 === 0 ? "짝수" : "홀수"}
+        </b>
     </div> 
   );
 }
